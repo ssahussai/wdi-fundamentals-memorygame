56af4666a16eebb8fbaa4b1a6b3b47e8eb6ad4f8
Salma Khan <hussainsalma37@gmail.com>
2019-10-14 14:47:04 -0500

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,51 +1,70 @@
 var cards = [
-{
-	rank: "queen",
-	suit: "hearts",
-	cardImage: "images/queen-of-hearts.png"
-},
-{
-	rank: "queen",
-	suit: "diamonds",
-	cardImage: "images/queen-of-diamonds.png"
-},
-{
-	rank: "king",
-	suit: "hearts",
-	cardImage: "images/king-of-hearts.png"
-},
-{
-	rank: "king",
-	suit: "diamonds",
-	cardImage: "images/king-of-diamonds.png"
-}
-];
-
-
-var cardsInPlay = [];
-cardsInPlay.push('cards[cardId]'.rank);
-
-function checkForMatch () {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-  		console.log("You found a match!");
-	} else {
-  		console.log("Sorry, try again.");
-	}
-}
-
-
-function flipCard(cardId) {
-if (cardsInPlay.length === 2) {
-	checkForMatch();
-}
-console.log("User flipped " + cards[cardId].rank);
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit);
-}
-
-flipCard(0);
-flipCard(2);
-
-
-
-
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+    ];
+    
+    
+    var cardsInPlay = [];
+    cardsInPlay.push('cards[cardId]'.rank);
+    
+    function checkForMatch () {
+        if (cardsInPlay[0] === cardsInPlay[1]) {
+              console.log("You found a match!");
+        } else {
+              console.log("Sorry, try again.");
+        }
+    }
+    
+    
+    function flipCard() {
+        var cardId = this.getAttribute('data-id');
+        this.setAttribute('src', cards[cardId].cardImage);
+        if (cardsInPlay.length === 2) {
+        checkForMatch();
+    }
+    console.log("User flipped " + cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
+    }
+    
+    
+    function createBoard() {
+        for (var i = 0; i < cards.length; i++) {
+            var cardElement = document.createElement('img');
+                cardElement.setAttribute('src', 'images/back.png');
+                cardElement.setAttribute('data-id', i);
+                document.getElementById('game-board').appendChild(cardElement);
+                cardElement.addEventListener('click', flipCard);
+        }
+    }
+    
+    createBoard();
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
